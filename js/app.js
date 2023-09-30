7645bf58003656b5efef00dd6e03b1253c4a9890
webdesign0001 <kh.at.e.ri.amir.h.osse.in@gmail.com>
2023-09-30 21:07:30 +0330

    addFeature(displayYear): Part1
    create getMaxYear & getMinYear & NumberConverter

---
+++ js/app.js
@@ -0,0 +1,42 @@
+// Variables
+
+
+
+// Events
+
+
+
+// Functions
+// User Interface Functions
+// Show Years
+function displayYears() {
+    // Convert to number
+    let
+        persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
+        arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g],
+        fixNumbers = function (str) {
+            if (typeof str === 'string') {
+                for (var i = 0; i < 10; i++) {
+                    str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
+                }
+            }
+            return parseInt(str);
+        };
+
+    // get now years
+    let curentYear = new Date().toLocaleDateString('fa-IR')
+
+    // Slice date
+    curentYear = curentYear.slice(0, 4)
+
+    // get max year
+    let maxYear = fixNumbers(curentYear)
+    console.log(maxYear);
+    
+    // get min year
+    let minYear = maxYear - 20
+    console.log(minYear);
+}
+
+
+displayYears()
