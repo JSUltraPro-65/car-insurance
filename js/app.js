2c3cb4c42000dff9de8eb1551571d70be91dd247
webdesign0001 <kh.at.e.ri.amir.h.osse.in@gmail.com>
2023-10-07 20:22:07 +0330

    add feature(displayMsg)

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 // Variables
-
+const form = document.querySelector('#request-quote')
 
 
 // Events
 document.addEventListener('DOMContentLoaded', afterLoad)
+document.addEventListener('submit', submitForm)
 
 
 // Functions
@@ -11,8 +12,41 @@ function afterLoad() {
     displayYears()
 }
 
+// submit form
+function submitForm(e) {
+    e.preventDefault();
 
-// User Interface Functions
+    // read value from the form
+    const make = document.querySelector('#make').value
+    const year = document.querySelector('#year').value
+    const level = document.querySelector('input[name="level"]:checked')
+
+    // check the value of fileds are correct
+    if (make === "" || year === "" || level === "") {
+        displayMsg('لطفاً مقادیر فرم را با دقت پر نمایید. با تشکر')
+    } else {
+        console.log('AllRight! :)');
+    }
+}
+
+
+// User Interface (UI) Functions
+// Display message box
+function displayMsg(msg) {
+    // create message box
+    const messageBox = document.createElement('div');
+    messageBox.classList = 'error'
+    messageBox.innerText = msg
+
+    // show message
+    form.insertBefore(messageBox, document.querySelector('.form-group'))
+
+    // remove message box
+    setTimeout(()=>{
+        document.querySelector('.error').remove()
+    },5000)
+}
+
 // Show Years
 function displayYears() {
     // Convert to number
@@ -36,14 +70,20 @@ function displayYears() {
 
     // get max year
     let maxYear = fixNumbers(curentYear)
-    console.log(maxYear);
 
     // get min year
     let minYear = maxYear - 20
-    console.log(minYear);
 
     // access to the select tag
     const selectYear = document.querySelector('#year')
+
+    // create first option tag for title
+    // create option tag
+    const optionTag = document.createElement('option')
+    optionTag.innerText = `- انتخاب -`;
+    // optionTag.value = ''
+    // append option to the selectYear
+    selectYear.appendChild(optionTag)
 
     // create for loop for making option tag
     for (let i = maxYear; i >= minYear; i--) {
